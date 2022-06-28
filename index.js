9c14eed01b7624f762dec0f757a41802a46fcd7d
fusion407 <fusion407@gmail.com>
2022-06-27 21:14:33 -0500

    commit tree e15cc9af0c21fdc2b25b972a19bf536d127c9442

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() + 1);
+
+let num3 = 9;
+let num4 = 22;
+let mod =  num4 % num3
+
+let max = Math.max(1, 2, 3, 20);
